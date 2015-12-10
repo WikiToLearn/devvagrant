5e0eecf1248a285fa0e3abcf3c23d23611803b95
Toma Luca <toma.luca95@gmail.com>
2015-12-10 13:15:10 +0100

    Added docker proxy

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,4 @@ sudo apt-get update
 sudo apt-get install -y curl
 sudo sh -c "curl -sSL get.docker.com | sh"
 sudo adduser vagrant docker
+docker run -tid -p 8080:8080 -p 3128:3128 --name devproxy --net host wikitolearn/devproxy:0.1
