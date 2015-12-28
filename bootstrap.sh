c051863e0b946a313050cf367e290ef502865b36
Toma Luca <toma.luca95@gmail.com>
2015-12-28 10:44:22 -0500

    Costum vagrant box start

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
+if [[ $(id -u) -ne 0 ]] ; then
+ sudo "$0"
+ exit $?
+fi
+getent passwd vagrant &> /dev/null
+if [[ $? -ne 0 ]] ; then
+ echo "Missing vagrant user"
+ exit 1
+fi
+echo root:vagrant | chpasswd
+echo vagrant:vagrant | chpasswd
+
 export DEBIAN_FRONTEND noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN true
-sudo apt-get update
-sudo apt-get dist-upgrade -y
-sudo apt-get install -y debconf-utils
+grep non-free /etc/apt/sources.list || sed -i 's/main/main non-free contrib/g' /etc/apt/sources.list
+apt-get update
+apt-get dist-upgrade -y
+apt-get install -y debconf-utils
 echo -e 'proftpd-basic\tshared/proftpd/inetd_or_standalone\tselect\tstandalone' | debconf-set-selections
-sudo apt-get install -y curl squid3 apache2 php5 proftpd-basic
-sudo chmod 777 /var/www/html/
-sudo chmod 777 /etc/squid3/
-sudo chmod 777 /etc/squid3/*
-sudo chmod 777 /etc/proftpd/
-sudo chmod 777 /etc/proftpd/*
-sudo sh -c "curl -sSL get.docker.com | sh"
-sudo adduser vagrant docker
+apt-get install -y curl squid3 apache2 php5 proftpd-basic virtualbox-guest-dkms sudo
+docker info || curl -sSL get.docker.com | sh
+adduser vagrant docker
 rm /var/www/html/* -vf
 a2enmod rewrite
-sed -i 's/80/8080/g' /etc/apache2/ports.conf
-sed -i 's/80/8080/g' /etc/apache2/sites-available/000-default.conf
+grep 8080 /etc/apache2/ports.conf || sed -i 's/80/8080/g' /etc/apache2/ports.conf
+grep 8080 /etc/apache2/sites-available/000-default.conf || sed -i 's/80/8080/g' /etc/apache2/sites-available/000-default.conf
 sed -i 's/None/All/g' /etc/apache2/apache2.conf
 
-{
-echo 'if [[ ! -f .restarted ]] ; then'
-echo ' for s in proftpd apache2 squid3 ; do'
-echo '  echo "Restarting "$s'
-echo '  sudo systemctl restart $s'
-echo ' done'
-echo ' touch .restarted'
-echo 'fi'
-echo 'export W2L_SKIP_OCG_DOCKER=1'
-} >> .bashrc
+cat files/index.php > /var/www/html/index.php
+cat files/.htaccess > /var/www/html/.htaccess
+cat files/squid.conf > /etc/squid3/squid.conf
+cat files/proftpd.conf > /etc/proftpd/proftpd.conf
+cat files/vagrantsudo > /etc/sudoers.d/vagrantsudo
+
+mkdir -p /home/vagrant/.ssh
+chmod 0700 /home/vagrant/.ssh
+wget --no-check-certificate \
+    https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub \
+    -O /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
